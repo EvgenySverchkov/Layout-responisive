1dcf7d8da1821fa295f2bbe28843e688e92ce7ab
EvgenySverchkov <47426436+EvgenySverchkov@users.noreply.github.com>
2020-04-19 20:14:03 +0300

    upd project

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
 $(function(){
 	$('.top-devices>div').on('click', function(e){
-		for(let i=0; i<3; i++){
-			let currElem = $($('.top-devices>div')[i]);
-			if(currElem.hasClass('active') && !$(this).is(currElem)){
-				currElem.removeClass('active');
-			}else{
-				if($(this).is(currElem)){
-					$(this).addClass('active');
-				}
-			}
-		}
+		if(!$(this).hasClass('active')){
+			$('.top-devices>div').removeClass('active');
+			$(this).addClass('active');
+		};
 	});
 	$('.header-menu__responsive-btn').on('click', function(){
 		$('.header-menu ul').slideToggle();
